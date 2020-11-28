8c965dd666abdaa29e3df276bb0ab43e2817e7ef
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-11-27 20:35:45 -0500

    initial commit.

---
+++ followAlong/src/reducers/index.js
@@ -0,0 +1,17 @@
+import { SET_TITLE, SET_EDITING, SET_NEWTEXTTITLE} from './../actions';
+
+const reducer = (state, action)=> {
+    switch(action.type){
+        case(SET_TITLE):
+            return({...state, title:action.payload});
+        case(SET_EDITING):
+            
+            return({...state, editing:action.payload});
+        case(SET_NEWTEXTTITLE):
+            return({...state, newTitleText:action.payload})
+        default:
+            return(state);
+    }
+}
+
+export default reducer;
